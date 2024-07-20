bd8891a6d35763293edeb9626ff75320beaa1e3e
sakthianand7 <sakthipraba1423@gmail.com>
2024-07-21 00:03:41 +0530

    Adding a new API for eligibility check

--- index.js
+++ index.js
@@ -19,24 +19,48 @@ Top 5 government schemes with high public interest or social impact India using 
   }
 }`;
 
+const ELIGIBILITY_CHECK_PROMPT = `
+Check the eligibility of the government scheme for the above profile using this JSON scheme:
+{ "type": "object",
+  "properties": {
+    "eligibility": { "type": "boolean" },
+    "reason": { "type": "string" }
+  }
+}
+`
+
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(express.json());
 
-app.get('/search', async (req, res) => {    
-    try {
-      const model = geminiAPI.getGenerativeModel({model: 'gemini-1.5-flash', generationConfig: { responseMimeType: "application/json" }});
-      const result = await model.generateContent(HOMEPAGE_PROMPT);
-      const response = await result.response;
-      let text = "[" + response.text() + "]";
-      let schemes = JSON.parse(text);
-      res.json(schemes);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error processing your request");
-    }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+app.get('/search', async (req, res) => {
+  try {
+    const model = geminiAPI.getGenerativeModel({ model: 'gemini-1.5-flash', generationConfig: { responseMimeType: "application/json" } });
+    const result = await model.generateContent(HOMEPAGE_PROMPT);
+    const response = await result.response;
+    let text = "[" + response.text() + "]";
+    let schemes = JSON.parse(text);
+    res.json(schemes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error processing your request");
+  }
+});
+app.post('/chat', async (req, res) => {
+  const model = geminiAPI.getGenerativeModel({ model: 'gemini-1.5-flash', generationConfig: { responseMimeType: "application/json" } });
+  const data = req.body;
+  const prompt = JSON.stringify(data) + "," + ELIGIBILITY_CHECK_PROMPT;
+  console.log(prompt);
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  console.log('Received data:', response.text());
+
+  res.status(201).send(JSON.parse(text));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
